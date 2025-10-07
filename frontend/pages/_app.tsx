147a3324b483ac3c6d9529333097e6516859bcfd
DMomot <dr.amid@mail.ru>
2025-10-07 22:29:59 +0200

    Add Google Analytics tracking (G-EDQ42E76WB)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,31 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import '../styles/globals.css';
 import MetaTags from '../src/components/MetaTags';
+
+const GA_TRACKING_ID = 'G-EDQ42E76WB';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
+      
       <MetaTags />
       <Component {...pageProps} />
     </>
