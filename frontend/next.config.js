ac4ca44cc8eb19c75590832c8aacdf5b8bee7ce0
DMomot <dr.amid@mail.ru>
2025-10-08 10:11:54 +0200

    Add sitemap-full.xml with winning config (text/xml + nosniff, no XML declaration)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -95,6 +95,23 @@ const nextConfig = {
         ],
       },
       {
+        source: '/sitemap-full.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/xml; charset=utf-8',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      {
         source: '/sitemap1.xml',
         headers: [
           {
