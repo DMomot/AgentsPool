61a131fd8a23f52594f2aee3d82f17962fe2fd84
DMomot <dr.amid@mail.ru>
2025-10-08 12:09:24 +0200

    Add sitemap-test.xml: NO indentation, NO changefreq, timezone format like marryme.ru

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -154,6 +154,23 @@ const nextConfig = {
         ],
       },
       {
+        source: '/sitemap-test.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/xml; charset=utf-8',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600',
+          },
+        ],
+      },
+      {
         source: '/sitemap.xml',
         headers: [
           {
