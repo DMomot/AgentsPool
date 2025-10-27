358e0a96b2ab228719ada3f55bd4c1daba3bb65f
DMomot <dr.amid@mail.ru>
2025-10-27 22:12:16 +0100

    feat: add 'Show All Categories' button on homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -100,6 +100,19 @@ export default function Home() {
               ))
             )}
           </div>
+
+          {/* Show All Button */}
+          <div className="text-center mt-12">
+            <Link
+              href="/categories"
+              className="inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 transition-colors"
+            >
+              Show All Categories
+              <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+              </svg>
+            </Link>
+          </div>
         </div>
       </section>
 
